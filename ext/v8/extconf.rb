5be57544002df7baa492bfd29f63ba2b752ee174
Ian Kelly <ltsquigs@gmail.com>
2016-06-20 14:15:23 -0700

    move it all over

--- ext/v8/extconf.rb
+++ ext/v8/extconf.rb
@@ -1,26 +1,2 @@
-require 'mkmf'
-
-have_library('pthread')
-have_library('objc') if RUBY_PLATFORM =~ /darwin/
-$CPPFLAGS += " -Wall" unless $CPPFLAGS.split.include? "-Wall"
-$CPPFLAGS += " -g" unless $CPPFLAGS.split.include? "-g"
-$CPPFLAGS += " -rdynamic" unless $CPPFLAGS.split.include? "-rdynamic"
-$CPPFLAGS += " -fPIC" unless $CPPFLAGS.split.include? "-rdynamic" or RUBY_PLATFORM =~ /darwin/
-
-CONFIG['LDSHARED'] = '$(CXX) -shared' unless RUBY_PLATFORM =~ /darwin/
-if CONFIG['warnflags']
-  CONFIG['warnflags'].gsub!('-Wdeclaration-after-statement', '')
-  CONFIG['warnflags'].gsub!('-Wimplicit-function-declaration', '')
-end
-if enable_config('debug')
-  $CFLAGS += " -O0 -ggdb3"
-end
-
-LIBV8_COMPATIBILITY = '~> 3.16.14'
-
-warn Dir.getwd
-
 pid = Process.spawn('ruby libv8conf.rb')
 Process.wait pid
-
-create_makefile('v8/init')
--- ext/v8/libv8conf.rb
+++ ext/v8/libv8conf.rb
@@ -1,4 +1,27 @@
-warn 'warn warn warn warn warn warn warn warn'
+require 'mkmf'
+
+have_library('pthread')
+have_library('objc') if RUBY_PLATFORM =~ /darwin/
+$CPPFLAGS += " -Wall" unless $CPPFLAGS.split.include? "-Wall"
+$CPPFLAGS += " -g" unless $CPPFLAGS.split.include? "-g"
+$CPPFLAGS += " -rdynamic" unless $CPPFLAGS.split.include? "-rdynamic"
+$CPPFLAGS += " -fPIC" unless $CPPFLAGS.split.include? "-rdynamic" or RUBY_PLATFORM =~ /darwin/
+
+CONFIG['LDSHARED'] = '$(CXX) -shared' unless RUBY_PLATFORM =~ /darwin/
+if CONFIG['warnflags']
+  CONFIG['warnflags'].gsub!('-Wdeclaration-after-statement', '')
+  CONFIG['warnflags'].gsub!('-Wimplicit-function-declaration', '')
+end
+if enable_config('debug')
+  $CFLAGS += " -O0 -ggdb3"
+end
+
+LIBV8_COMPATIBILITY = '~> 3.16.14'
+
 require '/usr/local/lib/ruby/gems/2.3.0/gems/libv8-3.16.14.15-x86_64-darwin-14/lib/libv8'
 
+warn Libv8::VERSION
+
 Libv8.configure_makefile
+
+create_makefile('v8/init')
