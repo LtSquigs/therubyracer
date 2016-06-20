65889cf622f625cf35e93d92af51c5333a13de53
Ian Kelly <ltsquigs@gmail.com>
2016-06-20 13:38:01 -0700

    version?

--- ext/v8/extconf.rb
+++ ext/v8/extconf.rb
@@ -28,9 +28,10 @@ rescue LoadError
 ensure
   warn 'warn warn warn warn warn warn warn warn'
   warn ENV['GEM_HOME']
- # require '/usr/local/lib/ruby/gems/2.3.0/gems/libv8-3.16.14.15-x86_64-darwin-14/lib/libv8'
+  require '/usr/local/lib/ruby/gems/2.3.0/gems/libv8-3.16.14.15-x86_64-darwin-14/lib/libv8'
 end
 
+warn Libv8::Version
 Libv8.configure_makefile
 
 create_makefile('v8/init')
