a8284e25712706380236297735a70b53428edaee
Ian Kelly <ltsquigs@gmail.com>
2016-06-20 13:36:48 -0700

    no libv8?

--- ext/v8/extconf.rb
+++ ext/v8/extconf.rb
@@ -28,7 +28,7 @@ rescue LoadError
 ensure
   warn 'warn warn warn warn warn warn warn warn'
   warn ENV['GEM_HOME']
-  require '/usr/local/lib/ruby/gems/2.3.0/gems/libv8-3.16.14.15-x86_64-darwin-14/lib/libv8'
+ # require '/usr/local/lib/ruby/gems/2.3.0/gems/libv8-3.16.14.15-x86_64-darwin-14/lib/libv8'
 end
 
 Libv8.configure_makefile
